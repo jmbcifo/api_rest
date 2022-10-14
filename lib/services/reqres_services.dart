6e5e5e0c41d7cc9db8ced20f0e073aa598a5e45e
Alex Escobar Ros <a.escobar.ros@gmail.com>
2022-10-14 11:41:59 +0200

    http response

--- lib/services/reqres_services.dart
+++ lib/services/reqres_services.dart
@@ -1,17 +1,25 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:api_rest/models/persons_model.dart';
 import 'package:http/http.dart' as http;
 
 class ServicesReqresApi {
+  PersonsModel personsModel = PersonsModel();
   void getReqRespServie() {
     var url = Uri.parse("https://reqres.in/api/users?page=2");
     print(url);
+
     http.get(url).then((response) {
       print(response.body);
-      final resReqRes = PersonsModel.fromJson(jsonDecode(response.body));
-      print(resReqRes);
-    });
+      personsModel = PersonsModel.fromJson(jsonDecode(response.body));
+      print(personsModel.data);
+      print(personsModel.data!.first.email);
+      personsModel.data?.forEach((element) {
+        print(element.email);
+      });
+    }).catchError((err) {});
+
     print("Hola desde mi API");
   }
 }
